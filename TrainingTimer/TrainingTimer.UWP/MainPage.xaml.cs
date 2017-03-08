76f451b1a9158a51eeeb1e94c72e89f2e533d96d
guyer <raphael.guye@gmail.com>
2017-03-08 09:00:25 +0100

    Import base project

---
+++ TrainingTimer/TrainingTimer.UWP/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using TrainingTimer.Model;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TrainingTimer.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public List<Exercise> Exercises
+        {
+            get
+            {
+                return new List<Exercise>()
+                {
+                    new Exercise() { Name = "Ex 1", Description = "Description 1", Duration = 30F, RepetitionNumber = 3 },
+                    new Exercise() { Name = "Ex 2", Description = "Description 2", Duration = 60F, RepetitionNumber = 4 },
+                    new Exercise() { Name = "Ex 3", Description = "Description 3", Duration = 30F, RepetitionNumber = 5 },
+                };
+            }
+        }
+    }
+}
---
+++ TrainingTimer/TrainingTimer/Model/Exercise.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingTimer.Model
+{
+    public class Exercise
+    {
+        /// <summary>
+        /// The name of the exercise
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The description of the excerise
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The number of repetiton of the exercise
+        /// </summary>
+        public int RepetitionNumber { get; set; }
+
+        /// <summary>
+        /// The duration of the exercice in seconds
+        /// </summary>
+        public float Duration { get; set; }
+    }
+}
